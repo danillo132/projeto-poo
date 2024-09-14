660770f64420b3b4d9c11c27ae2684caa6c7464f
danil <danil@host.docker.internal>
2024-09-14 14:57:25 -0300

    finalizar xeque e xeque-mate

--- ProjetoPOO/src/br/com/poo/entidades/Jogada.java
+++ ProjetoPOO/src/br/com/poo/entidades/Jogada.java
@@ -1,11 +1,13 @@
 package br.com.poo.entidades;
 
 import java.util.ArrayList;
+
 import java.util.List;
 
 public class Jogada {
 
 	private Jogador jogador;
+	private Jogador jogadorAdversario;
 	private Casa posicaoInicial;
 	private Casa posicaoFinal;
 	private Caminho caminho;
@@ -13,18 +15,21 @@ public class Jogada {
 	public Jogada() {
 	}
 
-	public Jogada(Jogador jogador, Casa posicaoInicial, Casa posicaoFinal) {
+	public Jogada(Jogador jogador,Jogador jogadorAdversario, Casa posicaoInicial, Casa posicaoFinal) {
 		this.jogador = jogador;
+		this.jogadorAdversario = jogadorAdversario;
 		this.posicaoInicial = posicaoInicial;
 		this.posicaoFinal = posicaoFinal;
 		caminho = new Caminho(posicaoInicial, posicaoFinal);
 	}
 
-	public boolean ehValida(Tabuleiro tabuleiro) {
+	public boolean ehValida(Tabuleiro tabuleiro, Jogador jogadorDoTurno) {
+		String corPeçasJogador = jogadorDoTurno.getPeças().get(0).getCor();
+		
 		if (tabuleiro.noLimite(posicaoInicial.getLinha(), posicaoInicial.getColuna())
 				&& tabuleiro.noLimite(posicaoFinal.getLinha(), posicaoFinal.getColuna())) {
-			if (jogador.getPeças().contains(caminho.casaInicial().getPeça())) {
-				if (posicaoFinal.getPeça() == null || !jogador.getPeças().contains(caminho.casaInicial().getPeça())) {
+			if (caminho.casaInicial().getPeça().getCor().equalsIgnoreCase(corPeçasJogador)) {
+				if (posicaoFinal.getPeça() == null || !caminho.casaFinal().getPeça().getCor().equalsIgnoreCase(corPeçasJogador)) {
 					if (caminho.estaLivre() || posicaoInicial.getPeça().getTipo().equalsIgnoreCase("C")) {
 						if (posicaoInicial.getPeça().movimentoValido(posicaoInicial.getColuna(),
 								posicaoInicial.getColuna(), posicaoFinal.getColuna(), posicaoFinal.getColuna())) {
@@ -41,8 +46,11 @@ public class Jogada {
 	}
 
 	public boolean ehXeque(Tabuleiro tabuleiro) {
-		// Implementar método
-		return true;
+		  boolean reiEmXeque = estaReiEmXeque(tabuleiro);
+		    if (!reiEmXeque) {
+		        return false;
+		    }
+		    return reiEmXeque;
 	}
 
 	public boolean ehXequeMate(Tabuleiro tabuleiro) {
@@ -52,18 +60,20 @@ public class Jogada {
 		if (!reiEmXeque) {
 			return false;
 		}
-		return true;
+		  Peça rei = encontrarCasaDoRei(tabuleiro).getPeça();
+		    boolean xequeMate = getPossiveisJogadas(rei, tabuleiro);
+		return xequeMate;
 	}
 
-	private boolean estaReiEmXeque(Tabuleiro tabuleiro, Jogador adversario) {
+	private boolean estaReiEmXeque(Tabuleiro tabuleiro) {
 		Casa casaDoRei = encontrarCasaDoRei(tabuleiro);
 		if (casaDoRei == null) {
 			return false;
 		}
 
 		for (Casa casaComPeçaAdversaria : getCasasComPeçasAdversarias(tabuleiro)) {
-			Jogada jogadaHipotetica = new Jogada(adversario, casaComPeçaAdversaria, casaDoRei);
-			if (jogadaHipotetica.ehValida(tabuleiro)) {
+			Jogada jogadaHipotetica = new Jogada(jogador, jogadorAdversario, casaComPeçaAdversaria, casaDoRei);
+			if (jogadaHipotetica.ehValida(tabuleiro, jogadorAdversario)) {
 				return true;
 			}
 		}
@@ -74,21 +84,69 @@ public class Jogada {
 	private Casa encontrarCasaDoRei(Tabuleiro tabuleiro) {
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 8; j++) {
-				Peça peca = tabuleiro.getTabuleiro()[i][j].getPeça();
-				if (peca instanceof Rei && peca.getCor().equals(jogador.getPeças().get(0).getCor())) {
+				Peça Peça = tabuleiro.getTabuleiro()[i][j].getPeça();
+				if (Peça instanceof Rei && Peça.getCor().equals(jogador.getPeças().get(0).getCor())) {
 					return tabuleiro.getTabuleiro()[i][j];
 				}
 			}
 		}
 		return null;
 	}
+	
+	private boolean getPossiveisJogadas(Peça peça, Tabuleiro tabuleiro) {
+	    if (peça == null) {
+	        return false; 
+	    }
+
+	    Casa casaDoRei = encontrarCasaDoRei(tabuleiro);
+	    if (casaDoRei == null) {
+	        return false;
+	    }
+
+	    int[] direcoes = {-1, 0, 1};
+	    for (int dx : direcoes) {
+	        for (int dy : direcoes) {
+	            if (dx == 0 && dy == 0) {
+	            	continue;
+	            }
+
+	            int novaLinha = casaDoRei.getLinha() + dx;
+	            int novaColuna = casaDoRei.getColuna() - dy;
+
+	            if (!tabuleiro.noLimite(novaLinha, (char) novaColuna)) {
+	                continue;
+	            }
+
+	            Casa novaCasa = tabuleiro.getTabuleiro()[novaLinha][novaColuna];
+	            Jogada jogadaHipotetica = new Jogada(jogador, jogadorAdversario, casaDoRei, novaCasa);
+
+	            if (jogadaHipotetica.ehValida(tabuleiro, jogador)) {
+	
+	                Peça rei = casaDoRei.getPeça();
+	                novaCasa.setPeça(rei);
+	                casaDoRei.setPeça(null);
+
+	              
+	                boolean reiAindaEmXeque = estaReiEmXeque(tabuleiro);
+	                if (!reiAindaEmXeque) {
+	                    return false; 
+	                }
+	                casaDoRei.setPeça(rei);
+	                novaCasa.setPeça(null);
+	            }
+	        }
+	    }
+
+	    return true; 
+	}
 
 	private List<Casa> getCasasComPeçasAdversarias(Tabuleiro tabuleiro) {
 		List<Casa> adversarios = new ArrayList<>();
+		String corPeçasJogador = jogador.getPeças().get(0).getCor();
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 8; j++) {
 				Casa casa = tabuleiro.getTabuleiro()[i][j];
-				if (casa != null && !casa.getPeça().getCor().equals(jogador.getPeças().get(0).getCor())) {
+				if (casa != null && !casa.getPeça().getCor().equalsIgnoreCase(corPeçasJogador)) {
 					adversarios.add(casa);
 				}
 			}
