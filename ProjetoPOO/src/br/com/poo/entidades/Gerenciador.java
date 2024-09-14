87e146aa54f5812b64a8f1838da3e8c1e030db03
GustavoAndreas <90465034+GustavoAndreas@users.noreply.github.com>
2024-09-14 13:46:35 -0300

    Gustavo - Gerenciador, Tabuleiro e Jogo
    Apenas alterações, classes ainda incompletas.

--- ProjetoPOO/src/br/com/poo/entidades/Gerenciador.java
+++ ProjetoPOO/src/br/com/poo/entidades/Gerenciador.java
@@ -1,19 +1,60 @@
 package br.com.poo.entidades;
 
+import java.util.Scanner;
+import java.util.logging.LogRecord;
+
 /**
- * Classe onde deve-se criar e disparar o jogo bem como carregar um jogo através de arquivo
+ * Classe onde deve-se criar e disparar o jogo bem como carregar um jogo através
+ * de arquivo
  */
 public class Gerenciador {
 
-	public static void main(String[] args) {
-		testesDesenvolvimento();
-	}
-	
-	
-	/**
-	 * Método responsável pelos testes exaustivos durante o desenvolvimento
-	 */
-	private static void testesDesenvolvimento() {
-		
-	}
+    public static void main(String[] args) {
+        testesDesenvolvimento();
+    }
+
+    private Jogo jogo;
+    Scanner ler = new Scanner(System.in);
+
+    public void menu() {
+
+        while (true) {
+            System.out.println("Bem vindo!");
+            System.out.println("Escolha uma das opcoes abaixo:");
+            System.out.println("1 - iniciar novo jogo");
+            System.out.println("2 - Carregar um jogo salvo");
+            System.out.println("3 - Sair");
+
+            int opcao = ler.nextInt();
+
+            if (opcao == 1) {
+                novoJogo();
+                jogo.iniciarJogo();
+            }
+            if (opcao == 2) {
+                carregarJogo();
+            }
+            if (opcao == 3) {
+                System.err.println("Encerrando...\n");
+                return;
+            }
+        }
+
+    }
+
+    private static void testesDesenvolvimento() {
+        /**
+         * Método responsável pelos testes exaustivos durante o desenvolvimento
+         */
+    }
+
+    public void novoJogo() {
+        jogo = new Jogo();
+        jogo.iniciarJogo();
+    }
+
+    public void carregarJogo() {
+
+    }
+
 }
