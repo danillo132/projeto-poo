130eaebe84ab44c9ab53b12b1942580b7ad655f7
danil <danil@host.docker.internal>
2024-09-14 11:43:15 -0300

    criar classe gerenciador para testar classes já feitas

---
+++ ProjetoPOO/src/br/com/poo/entidades/Gerenciador.java
@@ -0,0 +1,19 @@
+package br.com.poo.entidades;
+
+/**
+ * Classe onde deve-se criar e disparar o jogo bem como carregar um jogo através de arquivo
+ */
+public class Gerenciador {
+
+	public static void main(String[] args) {
+		testesDesenvolvimento();
+	}
+	
+	
+	/**
+	 * Método responsável pelos testes exaustivos durante o desenvolvimento
+	 */
+	private static void testesDesenvolvimento() {
+		
+	}
+}
